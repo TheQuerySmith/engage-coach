95e5559d74067c6f7dcbcbf6b2f480e6b9c0d46d
ericnatsmith <ericnatsmith@gmail.com>
2025-05-19 13:52:59 -0500

    refactor: Remove example documentation

--- components/task-lists/fetch-next-steps.tsx
+++ components/task-lists/fetch-next-steps.tsx
@@ -2,49 +2,6 @@ import { createClient } from '@/utils/supabase/server'
 import { TutorialStep } from "./tutorial-step";
 import { redirect } from "next/navigation";
 
-const create = `create table notes (
-  id bigserial primary key,
-  title text
-);
-
-insert into notes(title)
-values
-  ('Today I created a Supabase project.'),
-  ('I added some data and queried it from Next.js.'),
-  ('It was awesome!');
-`.trim();
-
-const server = `import { createClient } from '@/utils/supabase/server'
-
-export default async function Page() {
-  const supabase = await createClient()
-  const { data: notes } = await supabase.from('notes').select()
-
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
-}
-`.trim();
-
-const client = `'use client'
-
-import { createClient } from '@/utils/supabase/client'
-import { useEffect, useState } from 'react'
-
-export default function Page() {
-  const [notes, setNotes] = useState<any[] | null>(null)
-  const supabase = createClient()
-
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await supabase.from('notes').select()
-      setNotes(data)
-    }
-    getData()
-  }, [])
-
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
-}
-`.trim();
-
 export default async function FetchNextSteps() {
 const supabase = await createClient();
 
