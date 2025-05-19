f7a1714df00ae273ca5058c8a421bbb20757e83b
ericnatsmith <ericnatsmith@gmail.com>
2025-05-19 15:31:15 -0500

    feat(ui): Add info to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import FetchNextSteps from "@/components/task-lists/fetch-next-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+import AddNotification from "@/components/ui/notification-ribbon";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -15,17 +16,10 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-      </div>
-      
+    <div className="flex-1 w-full flex flex-col gap-12">      
       <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
+        <AddNotification message="Welcome to your dashboard! You can now start setting 
+        up your surveys by following the steps below" type="info" />
         <FetchNextSteps />
       </div>
     </div>
