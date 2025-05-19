698bb552ff9e95e530559d83b0249c6b14ea45ca
ericnatsmith <ericnatsmith@gmail.com>
2025-05-19 05:48:21 -0500

    fix: Change footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,14 +90,14 @@ export default function RootLayout({
             </div>
             <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
               <p>
-                Funded by{" "}
+                Created by{" "}
                 <a
-                  href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                  href=""
                   target="_blank"
                   className="font-bold hover:underline"
                   rel="noreferrer"
                 >
-                  Supabase
+                  QuerySmith
                 </a>
               </p>
               <ThemeSwitcher />
