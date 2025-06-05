0dec439bdc2d08e8737ca653b37760ac36811ca2
ericnatsmith <ericnatsmith@gmail.com>
2025-06-05 09:48:01 -0500

    feat: Add breadcrumb to anon pages

---
+++ app/(public-pages)/layout.tsx
@@ -0,0 +1,15 @@
+import Breadcrumb from "@/components/ui/breadcrumb";
+import AnonSidebar from "@/components/ui/anon-sidebar";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-1 max-w-7xl w-full mx-auto">
+      {/* Main content area */}
+      <div className="flex-1 p-5">
+        <Breadcrumb />
+        {children}
+      </div>
+    </div>
+  );
+}
+
