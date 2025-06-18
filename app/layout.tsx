6701fd5ca0d8fb67ac98934781515ce74c13ecbe
ericnatsmith <ericnatsmith@gmail.com>
2025-06-18 05:47:44 -0500

    fix: Remove footer (temp)

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
               </div>
             </nav>
             {children}
-            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+            {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
               <p>
                 Created by{" "}
                 <a
@@ -70,7 +70,7 @@ export default function RootLayout({
                 </a>
               </p>
               <ThemeSwitcher />
-            </footer>
+            </footer> */}
           </main>
         </ThemeProvider>
       </body>
